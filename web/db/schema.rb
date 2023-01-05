f027919a0bf36d08bccd72907f6051c39ced06c8
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-01-05 16:15:15 -0500

    Update shopify_app to 21.4.0

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_125631) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_200143) do
   create_table "shops", force: :cascade do |t|
     t.string "shopify_domain", null: false
     t.string "shopify_token", null: false
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_125631) do
     t.datetime "updated_at", null: false
     t.string "access_scopes"
     t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.bigint "shopify_user_id", null: false
+    t.string "shopify_domain", null: false
+    t.string "shopify_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "access_scopes"
+    t.index ["shopify_user_id"], name: "index_users_on_shopify_user_id", unique: true
   end
 
 end
