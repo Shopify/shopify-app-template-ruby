4bf7b7515fc2ee8a2072757e80055be5738b8e96
Remy Wang <remy.wang727@gmail.com>
2023-01-23 15:30:27 -0500

    Fix: schema version

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20220609125631) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_09_125631) do
   create_table "shops", force: :cascade do |t|
     t.string "shopify_domain", null: false
     t.string "shopify_token", null: false
