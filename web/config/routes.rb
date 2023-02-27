104de9ce4d784f86328e3d2deb1632fcdb06bba3
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-02-27 13:39:18 -0500

    Update routes.rb

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # them in web/frontend/vite.config.js
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   get "/api/products/count", to: "products#count"
   get "/api/products/create", to: "products#create"
 
