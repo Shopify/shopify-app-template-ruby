be892fd1657bc0052a98c85ab4aa1bde845ecabd
Alberto Vena <kennyadsl@gmail.com>
2024-07-10 17:27:12 +0200

    Update Rails bolierplate files with rails app:update
    This command proposes the new version of the Rails standard files.
    I checked them individually if we had any customizations, which have
    been kept in place.
    
    This commit also changes the load_defaults value to 7.1 so that we
    start loading all the new Rails defaults.

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_200143) do
+ActiveRecord::Schema[7.1].define(version: 2022_09_22_200143) do
   create_table "shops", force: :cascade do |t|
     t.string "shopify_domain", null: false
     t.string "shopify_token", null: false
