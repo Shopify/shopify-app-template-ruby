df12694d3e01b0646a2b38bb41b8bc5a5d676777
Elizabeth Kenyon <elizabeth.kenyon@shopify.com>
2024-07-17 16:29:10 -0500

    Add declarative webhooks processing
    The webhook registry in the package assumes that all webhooks to be processed will be defined in the config
    
    It also couples subscribing to processing
    
    This PR removes the registration loggic from the config.
    
    It uses the provided webhook verification concern with new controllers

--- web/app/jobs/shop_redact_job.rb
+++ web/app/jobs/shop_redact_job.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
 class ShopRedactJob < ActiveJob::Base
-  extend ShopifyAPI::Webhooks::Handler
-
-  class << self
-    def handle(topic:, shop:, body:)
-      perform_later(topic: topic, shop_domain: shop, webhook: body)
-    end
-  end
-
-  def perform(topic:, shop_domain:, webhook:)
+  def perform(shop_domain:, webhook:)
     shop = Shop.find_by(shopify_domain: shop_domain)
 
     if shop.nil?
