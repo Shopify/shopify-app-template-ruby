43a732422d81ecd3fb566faaa6b88b164b76d3ef
Andy Liu <andy.liu@shopify.com>
2024-11-11 09:35:11 -0500

    replace rest call with gql call

--- web/app/controllers/products_controller.rb
+++ web/app/controllers/products_controller.rb
@@ -3,9 +3,12 @@
 class ProductsController < AuthenticatedController
   # GET /api/products/count
   def count
-    product_count = ShopifyAPI::Product.count.body
+    product_count = ProductCounter.call(session: current_shopify_session, id_token: shopify_id_token)
     ShopifyAPI::Logger.info("Retrieved product count: #{product_count["count"]}")
     render(json: product_count)
+  rescue StandardError => e
+    logger.error("Failed to retrieve product count: #{e.message}")
+    render(json: { success: false, error: e.message }, status: e.try(:code) || :internal_server_error)
   end
 
   # POST /api/products
